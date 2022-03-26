eeca33e8f1732bed9707e4d277474c6548a8594a
kishore0404 <kishore0404@gmail.com>
2022-03-26 15:24:00 -0500

    commit

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { IonItem, IonLabel, IonInput, IonButton, IonCheckbox } from '@ionic/react';
+import { IonItem, IonLabel, IonInput, IonButton, IonCheckbox, IonIcon } from '@ionic/react';
+import { personCircleOutline } from "ionicons/icons";
 import { useForm } from 'react-hook-form';
 import { APIService } from '../../Services/api';
+import './login.css';
 
 const Login: React.FC = () => {
 
 
     return (
-        <div>
+        <div className="user-login">
+         <h2 className="login-heading">
+         	<IonIcon icon={personCircleOutline} size="large"></IonIcon>&nbsp;<span>Login</span>
+         </h2>
 	     <form className="ion-padding">
 		  <IonItem>
 		    <IonLabel position="floating">Username</IonLabel>
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,15 +1,12 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import Registration from '../components/Registration';
+import Header from '../components/Header';
 import './Tab3.css';
 
 const Tab3: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+       <Header />
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
